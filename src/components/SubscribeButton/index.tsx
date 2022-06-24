420f465f7db7baa3aa27f392eba64115d52d28e7
Estevão <estevaog.gomes@gmail.com>
2022-06-23 23:17:38 -0300

    [Function] Finished Stripe connection

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { signIn, useSession } from 'next-auth/react';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps{
@@ -7,16 +9,30 @@ interface SubscribeButtonProps{
 
 export function SubscribeButton({priceId}: SubscribeButtonProps){
     const {data: session} = useSession();
-    function HandleSubscribe(){
+    async function HandleSubscribe(){
         if(!session) {
             signIn('github')
             return
         }
+        try{
+            const response = await api.post('/subscribe')
 
+            const { sessionId } = response.data
+
+            const stripe = await getStripeJs()
+
+            stripe.redirectToCheckout({ sessionId:sessionId })
+            
+        }catch(err){
+            alert(err.message)
+        }
         
     }
     return(
-        <button type="button" className={styles.subscribeButton}>
+        <button type="button" 
+            className={styles.subscribeButton}
+            onClick={HandleSubscribe}
+        >
             Subscribe Now
         </button>
     );
